e8dd8c7ba8bb35ba2caa949548e365c8e68227e5
Ganesh <21ganesh2004@gmail.com>
2025-07-13 14:12:50 +0530

    Update Employee_Salary_Report.py

--- Employee_Salary_Report.py
+++ Employee_Salary_Report.py
@@ -1,33 +1,36 @@
-# Smart Billing System - Calculates total with GST and discount
+# Employee Salary Report - Calculates final salary and generates summary
 
-def Smart_Billing_System(name, qty, price):
-    total = qty * price
-    gst = total * 0.05  # 5% GST
-    if total >= 1000 :
-        discount = total * 0.10 
-        final=total+gst-discount
-    else:
-        0  # 10% discount if eligible
-    return total, gst, discount, final
+def Employee_Salary_Report(emp_name, base_salary, bonus, deduction):
+    final_salary = base_salary + bonus - deduction
+    return emp_name, base_salary, bonus, deduction, final_salary
 
-# List of purchased items
-items = [
-    ("Laptop Bag", 2, 750),
-    ("Wireless Mouse", 3, 400),
-    ("Notebook", 5, 100),
-    ("Pen Drive", 1, 900)
+# List of employees
+employees = [
+    ("Ganesh", 50000, 7000, 3000),
+    ("Riya", 62000, 5000, 4000),
+    ("Aman", 45000, 4500, 2000),
+    ("Sneha", 55000, 6000, 3500)
 ]
 
-# Print individual bill summary
-for name, qty, price in items:
-    total, gst, discount, final = Smart_Billing_System(name, qty, price)
-    print(f"\nItem: {name}")
-    print(f"Total: ₹{total}")
-    print(f"GST (5%): ₹{gst}")
-    print(f"Discount: ₹{discount}")
-    print(f"Final Amount: ₹{final}")
+salaries = []
 
-    #code example
+# Generate report for each employee
+for name, base, bonus, deduct in employees:
+    n, b, bo, d, final = Employee_Salary_Report(name, base, bonus, deduct)
+    print(f"\nName: {n}")
+    print(f"Base Salary: ₹{b}")
+    print(f"Bonus: ₹{bo}")
+    print(f"Deduction: ₹{d}")
+    print(f"Final Salary: ₹{final}")
+    salaries.append(final)
+
+# Print summary of salaries
+print("\n--- Salary Summary ---")
+print("Highest Salary:", max(salaries))
+print("Lowest Salary:", min(salaries))
+print("Average Salary:", round(sum(salaries)/len(salaries), 2))
+
+   #code example
 def  Employee_Salary_Report(emp_name,base_salary,bonus,deduction):
     final_salary=base_salary+bonus-deduction
     return emp_name,base_salary,bonus,deduction,final_salary
